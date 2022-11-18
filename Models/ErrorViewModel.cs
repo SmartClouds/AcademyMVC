fe6ea754f25922adb2e447cb33d60c38224de553
ajafarip <96071907+ajafarip@users.noreply.github.com>
2022-11-18 12:54:27 -0500

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AcademyMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
