cc8b3886c0014bf3c7d67d73b56dba3016b06593
ajafarip <96071907+ajafarip@users.noreply.github.com>
2022-11-18 15:08:55 -0500

    Update string length

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,15 +1,21 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
 
 namespace AcademyMVC.Data
 {
     public class ApplicationUser: IdentityUser
     {
+        [StringLength(250)]
         public string FirstName { get; set; }
+        [StringLength(250)]
         public string LastName { get; set; }
+        [StringLength(250)]
         public string Address1 { get; set; }
+        [StringLength(250)]
         public string Address2 { get; set; }
+        [StringLength(50)]
         public string PostCode { get; set; }
     }
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
