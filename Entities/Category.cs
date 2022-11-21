d95aa27c8b0baa81462f3d8722222e6060922e92
ajafarip <96071907+ajafarip@users.noreply.github.com>
2022-11-21 10:47:23 -0500

    Add Category Entity

---
+++ Entities/Category.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AcademyMVC.Entities
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(200,MinimumLength=2)]
+        public string Title { get; set; }
+        public string Description { get; set; }
+        [Required]
+        public string ThumbnailImagePath { get; set; }
+        [ForeignKey("CategoryId")]
+        public ICollection<CategoryItem> CategoryItems { get; set; }
+    }
+}
