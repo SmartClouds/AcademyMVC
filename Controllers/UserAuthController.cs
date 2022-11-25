629c3a47e357143da4efef71e101a36f29881aba
ajafarip <96071907+ajafarip@users.noreply.github.com>
2022-11-25 14:39:20 -0500

    Add Custom Login

---
+++ Controllers/UserAuthController.cs
@@ -0,0 +1,61 @@
+﻿
+using AcademyMVC.Data;
+using AcademyMVC.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AcademyMVC.Controllers
+{
+    public class UserAuthController : Controller
+    {
+        private  readonly UserManager<ApplicationUser> _userManager;
+        private  readonly SignInManager<ApplicationUser> _signManager;
+        private readonly ApplicationDbContext _context;
+
+        public UserAuthController(SignInManager<ApplicationUser> signManager, ApplicationDbContext context, UserManager<ApplicationUser> userManager)
+        {
+            _signManager = signManager;
+            _context = context;
+            _userManager = userManager;
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login( LoginModel loginModel)
+        {
+            loginModel.LoginInValid = "true";
+            if (ModelState.IsValid)
+            {
+                var result = await _signManager.PasswordSignInAsync(loginModel.Email,loginModel.Password,loginModel.RememberMe,lockoutOnFailure:false);
+                if (result.Succeeded)
+                {
+                    loginModel.LoginInValid = "";
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "Invalid Login attemp");
+                }
+
+            }
+            return PartialView("_UserLoginPartial",loginModel);
+        }
+        [AllowAnonymous]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Logout(string returnUrl = null)
+        {
+            await _signManager.SignOutAsync();
+            if (returnUrl != null)
+            {
+                return LocalRedirect(returnUrl);
+
+            }
+            else
+            {
+                return RedirectToAction("Index", "Home");
+            }
+        }
+    }
+}
