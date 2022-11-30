97d98b9c7b8d6e3ca51220d5102baae22b3055f3
ajafarip <96071907+ajafarip@users.noreply.github.com>
2022-11-30 16:37:07 -0500

    Add user selection in courses

--- Comparers/CompareUsers.cs
+++ Comparers/CompareUsers.cs
@@ -5,16 +5,22 @@ namespace AcademyMVC.Comparers
 {
     public class CompareUsers : IEqualityComparer<UserModel>
     {
-        public bool Equals(UserModel? x, UserModel? y)
-        {
-            if (y == null) return false;
-            if (x.Id == y.Id) return true;
-            return false;
-        }
+        
+            public bool Equals(UserModel x, UserModel y)
+            {
+                if (y == null) return false;
 
-        public int GetHashCode([DisallowNull] UserModel obj)
-        {
-           return obj.Id.GetHashCode();
-        }
+                if (x.Id == y.Id)
+                    return true;
+
+                return false;
+
+            }
+
+            public int GetHashCode([DisallowNull] UserModel obj)
+            {
+                return obj.Id.GetHashCode();
+            }
+        
     }
 }
